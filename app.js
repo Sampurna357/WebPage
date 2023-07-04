6bf1a123bbf1a39eacb886c83995fb51ddddbe94
Sampurna jena <100677782+Sampurna357@users.noreply.github.com>
2023-07-04 13:27:49 +0530

    today:04-07-2023

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ var generator=()=>{
    var sDetails={
      name:document.querySelector('#name').value,
     age:document.querySelector('#age').value,
-    gender:document.querySelector('.gender').value,
+    gender:document.querySelector("input[name=Sgender]:checked").value,
     clas:document.querySelector('#class').value,
-    mark:document.querySelector('.mark').value,
-    bimg:"https://static6.depositphotos.com/1002007/664/i/450/depositphotos_6646061-stock-photo-portrait-of-boy.jpg",
-    gimg:"https://images.unsplash.com/photo-1610882587765-f0439630a659?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybCUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+    mark:[]
+   };
+   console.log(sDetails);
+   sDetails.mark.push(parseInt(document.querySelector("#mark1").value));
+   sDetails.mark.push(parseInt(document.querySelector("#mark2").value));
+   sDetails.mark.push(parseInt(document.querySelector("#mark3").value));
+   sDetails.mark.push(parseInt(document.querySelector("#mark4").value));
+   sDetails.mark.push(parseInt(document.querySelector("#mark5").value));
+   sDetails.mark.push(parseInt(document.querySelector("#mark6").value));
+   console.log(sDetails.mark);
+   var gerGrade=()=>{
+           var r=0;
+           for(var i=0;i<sDetails.mark.length;i++){
+                r+=sDetails.mark[i];
+           }
+           return r/sDetails.mark.length;
    };
    var divTag=document.querySelector("div");
    divTag.setAttribute("class","HeaderReport");
    divTag.innerHTML="PROGRESS CARD";
    document.querySelector(".pageGenerator").append(divTag);
- 
    var ulTag=document.createElement('ul');
    ulTag.setAttribute("class","first");
    var litag1=document.createElement('li');
@@ -35,7 +47,21 @@ var generator=()=>{
    litag5.setAttribute("class","child");
    litag5.innerHTML="Student Mark:"+sDetails.mark;
    ulTag.append(litag5);
+   var avg=gerGrade();
+   if(avg>33){
+      var litag6=document.createElement('li');
+      litag6.setAttribute("class","child");
+   litag6.innerHTML="You Passed The Exam SuccessFully";
+   ulTag.append(litag6); 
+   }
+   else{
+      var litag6=document.createElement('li');
+      litag6.setAttribute("class","child");
+   litag6.innerHTML="You Failed The Exam ";
+   ulTag.append(litag6); 
+   }
    document.querySelector(".pageGenerator").append(ulTag);
+
    
 
 };
